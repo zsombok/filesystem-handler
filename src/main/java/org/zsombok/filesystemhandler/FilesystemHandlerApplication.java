a2e25a4a733053504e0ae4d6e853a3bb5a84a144
zsombok <zsombokd@gmail.com>
2024-04-30 12:36:43 +0200

    Set up google-formatter

--- src/main/java/org/zsombok/filesystemhandler/FilesystemHandlerApplication.java
+++ src/main/java/org/zsombok/filesystemhandler/FilesystemHandlerApplication.java
@@ -4,12 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FilesystemHandlerApplication
-{
+public class FilesystemHandlerApplication {
 
-  public static void main(String[] args)
-  {
-	SpringApplication.run(FilesystemHandlerApplication.class, args);
+  public static void main(String[] args) {
+    SpringApplication.run(FilesystemHandlerApplication.class, args);
   }
-
 }
