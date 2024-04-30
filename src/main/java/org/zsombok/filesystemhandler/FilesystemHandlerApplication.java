a3cc54004160bd52df715772c3689a75883e5d7a
zsombok <zsombokd@gmail.com>
2024-04-30 11:12:57 +0200

    Initial commit

---
+++ src/main/java/org/zsombok/filesystemhandler/FilesystemHandlerApplication.java
@@ -0,0 +1,15 @@
+package org.zsombok.filesystemhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesystemHandlerApplication
+{
+
+  public static void main(String[] args)
+  {
+	SpringApplication.run(FilesystemHandlerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/zsombok/filesystemhandler/FilesystemHandlerApplicationTests.java
@@ -0,0 +1,15 @@
+package org.zsombok.filesystemhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesystemHandlerApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
