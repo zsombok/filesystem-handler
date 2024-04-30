fb4373630735b0c20973f312c72cf5bee83cbd26
zsombok <zsombokd@gmail.com>
2024-04-30 22:23:24 +0200

    Add swagger-ui and description to endpoint

--- src/main/java/org/zsombok/filesystemhandler/controllers/FileController.java
+++ src/main/java/org/zsombok/filesystemhandler/controllers/FileController.java
@@ -1,11 +1,16 @@
 package org.zsombok.filesystemhandler.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.info.Info;
 import java.util.List;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.zsombok.filesystemhandler.models.GenerateRequest;
@@ -14,6 +19,9 @@ import org.zsombok.filesystemhandler.services.FileService;
 
 @Slf4j
 @RestController
+@RequestMapping(
+    produces = MediaType.APPLICATION_JSON_VALUE,
+    consumes = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class FileController {
 
@@ -36,6 +44,10 @@ public class FileController {
   }
 
   @PostMapping("/gen")
+  @Operation(
+      summary = "Generate folders and file",
+      description =
+          "Generates a series of subfolders with the given depth and a file with the given extension in the last folder. The folders and the file have UUID names. You find a form to post requests under the homepage of the application.")
   public ResponseEntity<HttpStatus> generateFile(@RequestBody GenerateRequest request) {
     try {
       fileService.generateFile(request);
